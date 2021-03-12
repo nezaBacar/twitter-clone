3911ed59b296f83d042f04bd3519e38d6c11aa26
zigakerec <ziga.kerec@gmail.com>
2021-03-12 14:00:53 +0100

    added readme and basic wrappers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+//
+// THOSE ARE THE TWEETS YOU NEED TO RENDER ON SCREEN, IMAGINE THOSE BEING SENT BACK FROM THE SERVER
+// 
+const TWEETS = [
+  {
+    id: 1,
+    tweet: 'This is the best tweet in the world. Even tho this is not actually a tweet. Oh wel...',
+    user: {
+      avatar: "https://static.hollywoodreporter.com/sites/default/files/2019/03/avatar-publicity_still-h_2019-compressed.jpg",
+      displayName: 'Mrs Avatar',
+      username: 'mrsavatar'
+    }
+  },
+  {
+    id: 2,
+    tweet: 'This is the best tweet in the world. Even tho this is not actually a tweet. Oh wel...',
+    user: {
+      avatar: "https://static.hollywoodreporter.com/sites/default/files/2019/03/avatar-publicity_still-h_2019-compressed.jpg",
+      displayName: 'Mrs Avatar',
+      username: 'mrsavatar'
+    }
+  },
+  {
+    id: 3,
+    tweet: 'This is the best tweet in the world. Even tho this is not actually a tweet. Oh wel...',
+    user: {
+      avatar: "https://static.hollywoodreporter.com/sites/default/files/2019/03/avatar-publicity_still-h_2019-compressed.jpg",
+      displayName: 'Mrs Avatar',
+      username: 'mrsavatar'
+    }
+  },
+  {
+    id: 4,
+    tweet: 'This is the best tweet in the world. Even tho this is not actually a tweet. Oh wel...',
+    user: {
+      avatar: "https://static.hollywoodreporter.com/sites/default/files/2019/03/avatar-publicity_still-h_2019-compressed.jpg",
+      displayName: 'Mrs Avatar',
+      username: 'mrsavatar'
+    }
+  },
+  {
+    id: 5,
+    tweet: 'This is the best tweet in the world. Even tho this is not actually a tweet. Oh wel...',
+    user: {
+      avatar: "https://static.hollywoodreporter.com/sites/default/files/2019/03/avatar-publicity_still-h_2019-compressed.jpg",
+      displayName: 'Mrs Avatar',
+      username: 'mrsavatar'
+    }
+  },
+  {
+    id: 6,
+    tweet: 'This is the best tweet in the world. Even tho this is not actually a tweet. Oh wel...',
+    user: {
+      avatar: "https://static.hollywoodreporter.com/sites/default/files/2019/03/avatar-publicity_still-h_2019-compressed.jpg",
+      displayName: 'Mrs Avatar',
+      username: 'mrsavatar'
+    }
+  },
+  {
+    id: 7,
+    tweet: 'This is the best tweet in the world. Even tho this is not actually a tweet. Oh wel...',
+    user: {
+      avatar: "https://static.hollywoodreporter.com/sites/default/files/2019/03/avatar-publicity_still-h_2019-compressed.jpg",
+      displayName: 'Mrs Avatar',
+      username: 'mrsavatar'
+    }
+  },
+  {
+    id: 8,
+    tweet: 'This is the best tweet in the world. Even tho this is not actually a tweet. Oh wel...',
+    user: {
+      avatar: "https://static.hollywoodreporter.com/sites/default/files/2019/03/avatar-publicity_still-h_2019-compressed.jpg",
+      displayName: 'Mrs Avatar',
+      username: 'mrsavatar'
+    }
+  },
+  {
+    id: 9,
+    tweet: 'This is the best tweet in the world. Even tho this is not actually a tweet. Oh wel...',
+    user: {
+      avatar: "https://static.hollywoodreporter.com/sites/default/files/2019/03/avatar-publicity_still-h_2019-compressed.jpg",
+      displayName: 'Mrs Avatar',
+      username: 'mrsavatar'
+    }
+  },
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <h1>Welcome to Shape Twitter. A place for your thoughts.</h1>
+
+
+      {/* THIS IS WHERE YOU START, YOUR TWEETS SHOULD APPEAR BELOW. */}
+
+
+    </Wrapper>
   );
 }
 
 export default App;
+
+// styled components that we use in majority of our projects
+// check them out, they're pretty cool
+const Wrapper = styled.div`
+  max-width: 960px;
+  width: 100%;
+  margin: 0 auto;
+`
